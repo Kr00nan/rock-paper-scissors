6947f24b6843067c4ccedee4a91f9e83c79eee68
Michael Kroon <kroonology@gmail.com>
2019-09-03 14:14:43 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var rock = document.getElementById('rock');
+
+var userChoice;
+var compChoice;
+
+rock.addEventListener('click', function(){
+  userChoice = this.id;
+  // get compChoice
+  // display those choices to the browser
+  // decides who wins
+});
