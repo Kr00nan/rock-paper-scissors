c24248973343c690f4e1917d84d7c6863f8a41ac
Michael Kroon <kroonology@gmail.com>
2019-09-03 22:45:15 -0600

    add rps logic

--- main.js
+++ main.js
@@ -9,27 +9,32 @@ var paper = document.getElementById('paper');
 var scissors = document.getElementById('scissors');
 var annnouncement = document.getElementById('announcement');
 
-rock.addEventListener('click', function(){
+rock.addEventListener('click', function () {
   userChoice = this.id;
   determineWinner(userChoice)
 });
 
-paper.addEventListener('click', function(){
+paper.addEventListener('click', function () {
   userChoice = this.id;
   determineWinner(userChoice);
 })
 
-scissors.addEventListener('click', function(){
+scissors.addEventListener('click', function () {
   userChoice = this.id;
   determineWinner(userChoice);
 })
 
-var determineWinner = function(userChoice){
+var determineWinner = function (userChoice) {
   var result = '';
-  compChoice = choices[Math.floor(Math.random()*3)];
-  if (userChoice === compChoice) {
-    result = 'It\'s a draw!';
+  compChoice = choices[Math.floor(Math.random() * 3)];
+  if(userChoice === compChoice) {
+    annnouncement.innerHTML = 'It\'s a draw!'; 
+  } else if ( (userChoice === 'rock' && compChoice === 'scissors') ||
+    (userChoice === 'paper' && compChoice === 'rock') ||
+    (userChoice === 'scissors' && compChoice === 'paper')) {
+    annnouncement.innerHTML = 'You win! Try again?';
+  } else {
+    annnouncement.innerHTML = 'You lose!';
   }
-  annnouncement.innerHTML = result;
 }
 
